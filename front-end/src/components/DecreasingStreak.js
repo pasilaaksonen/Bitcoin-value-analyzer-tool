5260fbd3dfe6ef6a50a8536d3091368a349b1fc5
pasilaaksonen <pasi.laaksonen@edu.turkuamk.fi>
2021-12-14 02:50:07 +0200

    Update DecreasingStreak.js

---
+++ front-end/src/components/DecreasingStreak.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DecreasingStreak({ decreasingStreak }) {
+    return (
+        <div>
+            <h2>Between the chosen dates:</h2>
+            <p>{decreasingStreak}</p>
+        </div>
+    )
+}
+
+export default DecreasingStreak
